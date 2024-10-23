f1903819fad614c0374474199e1b67015c592347
klrc <1440698245@qq.com>
2024-10-23 21:03:07 +0800

    Deploy

---
+++ auto_deploy.sh
@@ -0,0 +1,19 @@
+
+# Step 1: 压缩当前项目
+# package_name=$(git branch --show-current)
+cd 3rdparty/ultralytics-optimization
+package_name=person_veh_detection
+zip -r $package_name.zip . -x "*.git*"
+
+# Step 2: 使用 FTP 自动化删除旧文件并上传
+ftp -n -v <<END_SCRIPT
+open up
+user han.sun Elpsycongroo0
+binary
+delete $package_name.zip 
+put $package_name.zip  
+bye
+END_SCRIPT
+
+# Step 3: 清除压缩包
+rm $package_name.zip
